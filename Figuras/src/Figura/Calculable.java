f9fe6d2b08766757a96e9c42469eac8dcdbeae49
PaolaRabago <anaap.94@hotmail.com>
2023-10-15 22:30:18 -0700

    se actuliza repo con clases de polimorfismo

---
+++ Figuras/src/Figura/Calculable.java
@@ -0,0 +1,17 @@
+package Figura;
+
+public interface Calculable {
+
+	//Area, Volumen, Perimetro
+
+	
+	//Metodos de la interface
+	double calcularArea();
+	
+	double calcularVolumen();
+	
+	double calcularPerimetro();
+	
+	
+	
+}//interface calculable
